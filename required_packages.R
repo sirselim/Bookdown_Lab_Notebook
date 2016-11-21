83396bd6b279608a7dd066783f4e6a6794e221ae
sirselim <miles.benton84@gmail.com>
2016-11-22 11:22:37 +1300

    Large overhaul of book

---
+++ required_packages.R
@@ -0,0 +1,8 @@
+# load packages
+require(bookdown)
+require(rmarkdown)
+require(citr)
+require(knitr)
+require(pander)
+require(knitcitations)  # used to create citations and bibliography
+require(printr)
